d1192b3e33bc559db8164becbbfe1fdd467cda11
Nathan Long <him@nathanmlong.com>
2023-10-23 11:59:52 -0400

    Simplify

--- test/foo/things_text.exs
+++ test/foo/things_text.exs
@@ -3,18 +3,15 @@ defmodule Foo.ThingsTest do
   alias Foo.Things
 
   describe "create_thing" do
-    test "saves and retrieves maps" do
-      Things.create_thing(%{data: %{"hi" => "ho", "yo" => 2}})
-
-      [thing] = Things.list_things()
-      assert thing.data === %{"hi" => "ho", "yo" => 2}
-    end
-
     test "saves floats as expected" do
+      # compare to
+      # INSERT INTO things (data) VALUES ('{"a": 25030.0, "b": 25200.0}');
       Things.create_thing(%{data: %{a: 25200.0, b: 25030.0}})
 
       [thing] = Things.list_things()
+      # fails: first number is stored as an integer
       assert thing.data === %{"a" => 25200.0, "b" => 25030.0}
     end
   end
 end
+
