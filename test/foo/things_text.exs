f605ecf5f907cdb7cbde14a055a741be315fc78f
Nathan Long <him@nathanmlong.com>
2023-10-23 12:21:53 -0400

    add timestamps to example insert

--- test/foo/things_text.exs
+++ test/foo/things_text.exs
@@ -5,7 +5,7 @@ defmodule Foo.ThingsTest do
   describe "create_thing" do
     test "saves floats as expected" do
       # compare to
-      # INSERT INTO things (data) VALUES ('{"a": 25030.0, "b": 25200.0}');
+      # INSERT INTO things (data, inserted_at, updated_at) VALUES ('{"a": 25030.0, "b": 25200.0}', now(), now());
       Things.create_thing(%{data: %{a: 25200.0, b: 25030.0}})
 
       [thing] = Things.list_things()
